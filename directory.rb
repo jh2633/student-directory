754368d8e8c43e5e27ed85fcab9521b394fd6899
Joseph <joseph.jr@gmail.com>
2016-08-05 15:25:22 +0100

    refactoring as in step 10

--- directory.rb
+++ directory.rb
@@ -2,21 +2,21 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  puts "We are only intaking students with Dr. titles"
-  puts "You cannot input more than 12 characters"
-  students = []
+  #puts "We are only intaking students with Dr. titles"
+  #puts "You cannot input more than 12 characters"
+  @students = []
   name = gets.chomp.capitalize
 
   if name == ""
     exit
   end
   #exercise 12
-  if name[0] != "D" || name.length > 12
+  #if name[0] != "D" || name.length > 12
     #exercise 2 and 3, limites initial letter and length
-    puts "You have to have a Dr. qualification"
-    puts "And the name cannot be more than 12 characters"
-    name = gets.chomp
-  end
+  #  puts "You have to have a Dr. qualification"
+  #  puts "And the name cannot be more than 12 characters"
+  #  name = gets.chomp
+  #end
 
   puts "What is their hobby?"
   hobby = gets.chomp
@@ -31,11 +31,11 @@ def input_students
     cohort = "November"
   end
   while !name.empty? do
-    students << {name: name.capitalize, cohort: cohort, hobby: hobby, height: height}
-    if students.count == 1
-      puts "Now we have #{students.count} student"
+    @students << {name: name.capitalize, cohort: cohort, hobby: hobby, height: height}
+    if @students.count == 1
+      puts "Now we have #{@students.count} student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
   #exercise 9, plural and singular student(s)
     name = gets.chomp
@@ -47,7 +47,7 @@ def input_students
       height = gets.chomp
     end
   end
-  students
+  @students
 end
 
 def header
@@ -55,8 +55,8 @@ puts "The students of Villains Academy"
 puts "-------------"
 end
 
-def print(names)
-  names.each_with_index do |student, index|
+def print_student_names
+  @students.each_with_index do |student, index|
   puts "#{(index+1)}. #{student[:name]} (#{student[:cohort]} cohort)"
   puts "Hobby: #{student[:hobby].capitalize} Height: #{student[:height]}cm"
   #added each with index, increase index by one to offset 0
@@ -64,36 +64,44 @@ def print(names)
   end
 end
 
-def print_footer(names)
-  if names.count == 1
-    puts "Overall, we have #{names.count} great student"
+def print_footer
+  if @students.count == 1
+    puts "Overall, we have #{@students.count} great student"
   else
-    puts "Overall, we have #{names.count} great students"
+    puts "Overall, we have #{@students.count} great students"
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  header
+  print_student_names
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
   end
 end
 
 def interactive_menu
-  students = []
+  @students = []
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 interactive_menu
