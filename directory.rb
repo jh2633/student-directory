2058c8f400b6f8bd933c653e2a6a097fe390c9a8
Joseph <joseph.jr@gmail.com>
2016-08-05 16:09:24 +0100

    save csv feature and gitignore

--- directory.rb
+++ directory.rb
@@ -72,9 +72,21 @@ def print_footer
   end
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list of students and their cohort to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -90,6 +102,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit # this will cause the program to terminate
   else
